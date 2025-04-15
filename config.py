2dc0e34104a960764fe96cf1fd5db3c53a0f9bf4
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2025-04-15 15:22:45 +0530

    Update config.py
    SQLALCHEMY_DATABASE_URI - changed

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tiger@localhost/secure_syscall_db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///syscall.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = '9a38d675fecc9eb835a0e882e6fc31cb'
